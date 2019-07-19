17cc1500ab64feaf3326455b1a4348a0623e0a6a
Stefanie Moy <stefaniesmoy@gmail.com>
2019-07-19 21:24:55 +0000

    Changed server to serve index.html for views.

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const path = require('path');
+
+app.use(express.static('build'));
+
 app.use('/api/quotes', require('./routes/quote-routes'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!..!!');
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT, () => {
