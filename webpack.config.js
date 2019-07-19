3ac960a6fb39bc0afd2382e65705fe8e5132ac87
Stefanie Moy <stefaniesmoy@gmail.com>
2019-07-19 21:50:01 +0000

    Webpack.config.js indicates all output files go into build directory.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }, 
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: 'css-loader'
+          }
+        )
+      }
+    ]
+  }, 
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      filename: "/index.html"
+    }),
+    new ExtractTextPlugin({filename: 'style.css'})
+  ]
+};
+
+module.exports = config;
