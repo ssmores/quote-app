429df4b2bd0adcb33a374394a81769980fbbee71
Stefanie Moy <stefaniesmoy@gmail.com>
2019-07-19 20:34:17 +0000

    New route that pulls all from db.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use('/api/quotes', require('./routes/quote-routes'));
+
 app.get('/', (req, res) => {
     res.send('Hello World!!..!!');
 });
