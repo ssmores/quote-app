2ed290f75ce96b14dac00f6fbf27fd1777475a6e
Stefanie Moy <stefaniesmoy@gmail.com>
2019-07-26 01:20:20 +0000

    Changed App to include home and quote components and styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import './App.css';
 
 class App extends Component {
   render() {
       return (
+        <Router>
           <div className="App">
-            Hello Worlds!
+            <Route exact path="/" component={Home} />
+            <Route exact path="/:id" component={Quote} />
           </div>
+        </Router>
       )
   }
 };
